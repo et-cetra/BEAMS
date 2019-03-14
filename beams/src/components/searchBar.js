e9bc2054d2f7df3ec96e54bd31a02e7d3254d08a
et-cetra <mar.shmalko@gmail.com>
2019-03-14 20:26:27 +1100

    Initial try

---
+++ beams/src/components/searchBar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './searchBar.css';
+
+class searchBar extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default searchBar;
