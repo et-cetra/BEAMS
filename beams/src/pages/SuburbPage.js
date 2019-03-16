295bbc1e8fa40e08639ca4318cd1edcde1e805f2
Mariya Shmalko <mar.shmalko@gmail.com>
2019-03-17 09:25:50 +1100

    Map added to suburb page

--- beams/src/pages/SuburbPage.js
+++ beams/src/pages/SuburbPage.js
@@ -3,9 +3,16 @@ import React from 'react';
 class SuburbPage extends React.Component {
     render() {
         const suburb = this.props.suburb;
+        const url = `https://www.google.com/maps/embed/v1/place?key=AIzaSyDIMGCB2qSD9qIB0mrZu0uGEmZlc9e8m-Y&q=${suburb}`;
       return (
         <div className="App">
           <h4>Here is the suburb page for {suburb}!</h4>
+            <iframe
+                width="600"
+                height="450"
+                frameborder="0"
+                src={url} allowfullscreen>
+            </iframe>
           <button onClick={this.props.onStartOver}>Home</button>
         </div>
       );
