ff40be654fddca0f9189f9e5febe409fb7121474
Mariya Shmalko <mar.shmalko@gmail.com>
2019-04-14 21:54:43 +1000

    Created demographics.js

---
+++ beams/src/components/Demographics.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Grid, AppBar, Toolbar, Typography, createMuiTheme, MuiThemeProvider, Link } from '@material-ui/core';
+import 'typeface-roboto';
+import mBannerSm from '../assets/ic_banner_small.png'
+import QuickSearch from "./QuickSearch"
+import PinIcon from '@material-ui/icons/LocationOn'
+
+class Demographics extends React.Component {
+  render() {
+    return (
+        <div></div>
+    );
+  }
+}
+
+export default Demographics;
