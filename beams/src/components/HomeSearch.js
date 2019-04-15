3586d6588b1a5f0d67348ad0584ae4bff06bf7a1
Mariya Shmalko <mar.shmalko@gmail.com>
2019-04-15 14:08:14 +1000

    Added highlighting to first search option.

--- beams/src/components/HomeSearch.js
+++ beams/src/components/HomeSearch.js
@@ -2,13 +2,13 @@ import React from 'react';
 import SearchIcon from "@material-ui/icons/Search";
 import { Grid, Paper, InputBase, Card, ListItem, Divider } from '@material-ui/core';
 import './HomeSearch.css'
-import PlacesAutocomplete, {geocodeByAddress, getLatLng} from 'react-places-autocomplete';
+import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete';
 
 class HomeSearch extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = { city: '', query: '', address: ''};
+        this.state = { city: '', query: '', address: '' };
     }
 
     handleChange = address => {
@@ -16,7 +16,6 @@ class HomeSearch extends React.Component {
     };
 
     handleSelect = address => {
-        console.log('Address:', address);
         this.props.onSelect(address);
         geocodeByAddress(address)
             .then(results => getLatLng(results[0]))
@@ -25,46 +24,46 @@ class HomeSearch extends React.Component {
     };
 
     render() {
-      return (
-        <Paper className="HomeSearchBox">
-        <Grid container direction="row" alignItems="center">
-            <Grid item xs={1}>
-                <SearchIcon className="HomeSearchIcon" fontSize="large"/>
-            </Grid>
-            <PlacesAutocomplete
-            value={this.state.address}
-            onChange={this.handleChange}
-            onSelect={this.handleSelect}
-            searchOptions={{types: ['(cities)'], componentRestrictions: {country: 'au'}}}>
-                {({ getInputProps, suggestions, getSuggestionItemProps, i }) => (
-                <div>
-                <InputBase {...getInputProps({
-                    placeholder: 'Search for Suburb, Postcode...',
-                    className: 'HomeSearch',
-                  })}/>
-                <Card className="AutocompleteCardHome">
-                    {suggestions.map(suggestion => {
-                    const className = suggestion.active ? 'suggestion-item--active' : 'suggestion-item';
-                    const style = suggestion.active ? 
-                    { backgroundColor: '#fca064', cursor: 'pointer' } : { backgroundColor: '#FFFFFF', cursor: 'pointer' };
-                    
-                    return (
-                    <div key={suggestion.id}>
-                        <ListItem className="SearchResultsHome" {...getSuggestionItemProps(suggestion, { className, style })}>
-                            <span>{suggestion.description}</span>
-                        </ListItem>
-                        <Divider></Divider>
-                    </div>
-                    );
-                })}
-                </Card>                  
-                </div>
-            )}</PlacesAutocomplete>
-        </Grid>
-        </Paper>
-      );
+        return (
+            <Paper className="HomeSearchBox">
+                <Grid container direction="row" alignItems="center">
+                    <Grid item xs={1}>
+                        <SearchIcon className="HomeSearchIcon" fontSize="large" />
+                    </Grid>
+                    <PlacesAutocomplete
+                        value={this.state.address}
+                        onChange={this.handleChange}
+                        onSelect={this.handleSelect}
+                        highlightFirstSuggestion={true}
+                        searchOptions={{ types: ['(cities)'], componentRestrictions: { country: 'au' } }}>
+                        {({ getInputProps, suggestions, getSuggestionItemProps, i }) => (
+                            <div>
+                                <InputBase {...getInputProps({
+                                    placeholder: 'Search for Suburb, Postcode...',
+                                    className: 'HomeSearch',
+                                })} />
+                                <Card className="AutocompleteCardHome">
+                                    {suggestions.map(suggestion => {
+                                        const className = suggestion.active ? 'suggestion-item--active' : 'suggestion-item';
+                                        const style = suggestion.active ?
+                                            { backgroundColor: '#fca064', cursor: 'pointer' } : { backgroundColor: '#FFFFFF', cursor: 'pointer' };
+                                        return (
+                                            <div key={suggestion.id}>
+                                                <ListItem className="SearchResultsHome" {...getSuggestionItemProps(suggestion, { className, style })}>
+                                                    <span>{suggestion.description}</span>
+                                                </ListItem>
+                                                <Divider></Divider>
+                                            </div>
+                                        );
+                                    })}
+                                </Card>
+                            </div>
+                        )}</PlacesAutocomplete>
+                </Grid>
+            </Paper>
+        );
     }
-  }
+}
 
 export default HomeSearch;
 
