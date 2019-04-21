f15c55959a5ad830c67ca18b61aca14f11d70123
bei <beichen7598@gmail.com>
2019-04-21 19:14:51 +1000

    dev page progress

--- beams/src/pages/DevPage.js
+++ beams/src/pages/DevPage.js
@@ -4,31 +4,69 @@ import './DevPage.css'
 import { Grid, Typography, Grow } from '@material-ui/core';
 import HomeSearch from '../components/HomeSearch';
 import avatar from '../assets/avatar2.png'
+import bei from'../assets/bei.png'
+import mariya from'../assets/mariya.png'
 
 //card ref
 // https://www.w3schools.com/w3css/tryit.asp?filename=tryw3css_templates_website&stacked=h
+// linkedin <a href="https://www.linkedin.com/in/bei-chen-994922142/"><i class="fa fa-linkedin"></i></a>
+
 class DevPage extends React.Component {
     render() {
       return (
         <div>
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
+            <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"/>
 
 
-            <Grid container direction="column" justify="space-evenly" alignItems="center">
-
-              <Grid item className="card">
+            <div class="w3-row-padding">
+              <div class="w3-col m2 w3-center"><p></p>
+              <div className="card">
                   <img src={avatar} style={{width:100}}/>
-                  <h3>Example</h3>
+                  <h3>Mozamel Anwary</h3>
+
                   <p>The University of New South Wales</p>
-                  <a href="#"><i class="fa fa-linkedin"></i></a>
-                  <a href="#"><i class="fa fa-facebook"></i></a>
-                  <p><button>Contact</button></p>
-              </Grid>
+                  <a href="https://github.com/atude"><i class="fa fa-github"></i></a>
+              </div>
+              </div>
+              <div class="w3-col m2 w3-center"><p></p>
+              <div className="card">
+                  <img src={bei} style={{width:100}}/>
+                  <h3>Bei Chen</h3>
 
-            </Grid>
+                  <p>The University of New South Wales</p>
+                  <a href="https://github.com/bei98"><i class="fa fa-github"></i></a>
+              </div>
+              </div>
+              <div class="w3-col m2 w3-center"><p></p>
+              <div className="card">
+                  <img src={avatar} style={{width:100}}/>
+                  <h3>Paul Grace</h3>
 
+                  <p>The University of New South Wales</p>
+                  <a href="https://github.com/PaulGrace1200"><i class="fa fa-github"></i></a>
+              </div>
+              </div>
+              <div class="w3-col m2 w3-center"><p></p>
+              <div className="card">
+                  <img src={avatar} style={{width:100}}/>
+                  <h3>Sumayyah Ho</h3>
 
+                  <p>The University of New South Wales</p>
+                  <a href="https://github.com/z5210234"><i class="fa fa-github"></i></a>
+              </div>
+              </div>
+              <div class="w3-col m2 w3-center"><p></p>
+              <div className="card">
+                  <img src={mariya} style={{width:100}}/>
+                  <h3>Mariya Shmalko</h3>
 
+                  <p>The University of New South Wales</p>
+                  <a href="https://github.com/et-cetra"><i class="fa fa-github"></i></a>
+              </div>
+              </div>
+
+            </div>
         </div>
       );
     }
