c3846731a7234d846618e8e1c2c6a9d5240409fc
mozc <mozamel.anwary1@gmail.com>
2019-04-27 14:16:20 +1000

    Schools done for single suburb

--- beams/src/components/demographics/Schools.js
+++ beams/src/components/demographics/Schools.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '../../App.css';
 import '../../pages/SuburbPage.css';
 import { getLocation, getSchools } from '../../utils.js';
-import { ExpansionPanel, ExpansionPanelSummary, ExpansionPanelDetails, Typography, CircularProgress, Chip } from '@material-ui/core';
+import { ExpansionPanel, ExpansionPanelSummary, ExpansionPanelDetails, Typography, CircularProgress, Paper, Chip, Card, FormControlLabel, FormControl, FormLabel, RadioGroup, Radio, FormGroup } from '@material-ui/core';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import mSchools from '../../assets/ic_schools.png'
 
@@ -14,6 +14,10 @@ class Schools extends React.Component {
         isLoaded: false,
         schoolData: [],
         schoolData2: [],
+
+        chGender: "All",
+        chType: "All",
+        chEducationLevel: "All",
     };
   }
 
@@ -39,31 +43,51 @@ class Schools extends React.Component {
     }
   }
 
-  state = {
-    primary: false,
-    secondary: false,
-  };
-  
   handleChange = name => event => {
-    this.setState({ [name]: event.target.checked });
+    this.setState({ [name]: event.target.value });
   };
 
-  getSingleList = (schoolData) => {
+  getSingleList = (getData) => {
+    var schoolData = getData;
+    const { chGender, chType, chEducationLevel } = this.state;
+    console.log(chGender);
+
+    if(chGender == "Girls") schoolData = schoolData.filter(item => item.gender === "Girls");
+    if(chGender == "Boys") schoolData = schoolData.filter(item => item.gender === "Boys");
+    if(chType == "Public") schoolData = schoolData.filter(item => item.type === "Government");
+    if(chType == "Private") schoolData = schoolData.filter(item => item.type === "Private");
+    if(chEducationLevel == "Primary") schoolData = schoolData.filter(item => item.educationLevel === "Primary");
+    if(chEducationLevel == "Secondary") schoolData = schoolData.filter(item => item.educationLevel === "Secondary");
+
+    if(schoolData.length == 0) 
+      return (
+        <div>
+        <br/><br/><br/><br/>
+        <Typography align="center" variant="button" style={{fontSize: "15px"}}>
+          No schools found nearby
+        </Typography>
+        <br/><br/><br/><br/>
+        </div>
+      );
     return(
       schoolData.map(
-      (item, i) => (
-      <ExpansionPanel key={item.id}>
-      <ExpansionPanelSummary expandIcon={<ExpandMoreIcon/>}>
-        <Typography>{item.name}</Typography>
-      </ExpansionPanelSummary>
-      <ExpansionPanelDetails>
-        <Typography>
-        <b>Type</b> {item.type} <br/>
-        <b>Education Level</b> {item.educationLevel} <br/>
-        <b>Gender</b> {item.gender}
-        </Typography>
-      </ExpansionPanelDetails>                       
-      </ExpansionPanel>))
+      (item) => (
+
+      <div key={item.id} style={{paddingTop: "5px"}}>
+      <Card className="SchoolCard">
+        <a href={item.websiteUrl} target="_blank" rel="noopener noreferrer">
+          <Typography className="SchoolText" inline variant="h5" style={{fontSize: "15px"}}>
+          {item.name}
+          </Typography>
+        </a>
+        <Chip className="SchoolTags" label={item.type}/>
+        <Chip className="SchoolTags" label={item.educationLevel}/>
+        <Chip className="SchoolTags" label={item.gender}/>
+      </Card>
+      </div>
+      
+      )
+      )
     );
   }
 
@@ -123,8 +147,8 @@ class Schools extends React.Component {
         return <div><CircularProgress size={60} color="primary"/></div>;
     } else {
       return (
-        <ExpansionPanel className="SchoolPanel" style={{boxShadow: 'none'}}>
-          <ExpansionPanelSummary style={{marginLeft: '-24px'}} expandIcon={<ExpandMoreIcon/>}>
+        <Card className="SchoolPanel" style={{boxShadow: 'none'}}>
+          <ExpansionPanelSummary style={{marginLeft: '-24px', cursor: "inherit"}}>
             <img src={mSchools} className="IconDef" alt="ic_school"/>
             <Typography align="inherit" inline className="SchoolsText" 
             style={{ fontSize: 24 }} variant="h1" color="inherit">
@@ -142,12 +166,46 @@ class Schools extends React.Component {
 
             </Typography>
           </ExpansionPanelSummary>
+          
+          <FormGroup row className="SchoolControls">
+          <FormControl component="fieldset">
+            <div className="SchoolControlBlock">
+            <FormLabel component="legend">Gender</FormLabel>
+            <RadioGroup row aria-label="Gender" name="gender1" value={this.state.chGender} onChange={this.handleChange('chGender')}>
+              <FormControlLabel value="All" control={<Radio/>} label="All"/>
+              <FormControlLabel value="Girls" control={<Radio/>} label="Girls"/>
+              <FormControlLabel value="Boys" control={<Radio/>} label="Boys"/>
+            </RadioGroup>
+            </div>
+          </FormControl>  
+          <FormControl component="fieldset">
+            <div className="SchoolControlBlock">
+            <FormLabel component="legend">Type</FormLabel>
+            <RadioGroup row aria-label="Type" name="type1" value={this.state.chType} onChange={this.handleChange('chType')}>
+              <FormControlLabel value="All" control={<Radio/>} label="All"/>
+              <FormControlLabel value="Public" control={<Radio/>} label="Public"/>
+              <FormControlLabel value="Private" control={<Radio/>} label="Private"/>
+            </RadioGroup>
+            </div>
+          </FormControl>  
+          <FormControl component="fieldset">
+            <div className="SchoolControlBlock">
+            <FormLabel component="legend">Education Level</FormLabel>
+            <RadioGroup row aria-label="Education Level" name="level1" value={this.state.chEducationLevel} onChange={this.handleChange('chEducationLevel')}>
+              <FormControlLabel value="All" control={<Radio/>} label="All"/>
+              <FormControlLabel value="Primary" control={<Radio/>} label="Primary (K-6)"/>
+              <FormControlLabel value="Secondary" control={<Radio/>} label="Secondary (7-12)"/>
+            </RadioGroup>
+            </div>
+          </FormControl>  
+          </FormGroup>
 
           <div className="ScrollMenu">
+          {console.log(schoolData)}
           {!isCompare ? this.getSingleList(schoolData) 
             : this.getMergedList(schoolData, schoolData2, suburb, suburb2)}
           </div>
-          </ExpansionPanel>
+          </Card>
       );
     }
   }
