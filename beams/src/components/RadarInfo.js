331b03d9973bd928fcb86c01f003b59dab058e4b
Mariya Shmalko <mar.shmalko@gmail.com>
2019-04-30 23:44:25 +1000

    Editted text

--- beams/src/components/RadarInfo.js
+++ beams/src/components/RadarInfo.js
@@ -32,7 +32,12 @@ class Info extends React.Component {
           {({ TransitionProps }) => (
             <Fade {...TransitionProps} timeout={350}>
               <Paper className="PopperInfo">
-                Information for this chart is sourced from state crime records, ICSEA school ratings and ABS.
+                <p>Information for this chart is sourced from state crime records, ICSEA school ratings and ABS.</p>
+                <p><b>Affordability</b> is found through low, median and highest prices within suburbs.</p>
+                <p><b>Education Quality</b> is caluclated through the ICSEA school rating.
+                The ICSEA scale is an index of community socio-educational advantage for school attendees.</p>
+                <p><b>Safety</b> is the crime rate of the suburb for the most recent 12 month period.</p>
+                <p><b>Socio-Economics</b> rating is given by ABS.</p>
               </Paper>
             </Fade>
           )}
